ee3aa73afd0a89b5310f7d36adca6c68252ae23e
rezolyt <rezolyt@gmail.com>
2022-07-05 13:03:41 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,133 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form class="card" @submit.prevent="submitHandler">
+      <h1>Анкета на Vue разработчика!</h1>
+<!--      <div class="form-control" :class="{invalid: errors.name}">-->
+<!--        <label for="name">Как тебя зовут?</label>-->
+<!--        <input-->
+<!--            type="text"-->
+<!--            id="name"-->
+<!--            placeholder="Введи имя"-->
+<!--            v-model.trim="name"-->
+<!--        >-->
+<!--        <small v-if="errors.name">{{ errors.name }}</small>-->
+<!--      </div>-->
+
+      <app-input
+        placeholder="Введи имя"
+        :error="errors.name"
+        label="Как тебя зовут?"
+        v-model:value="name"
+      ></app-input>
+
+      <div class="form-control">
+        <label for="age">Выбери возраст</label>
+        <input
+            type="number"
+            id="age"
+            max="70"
+            v-model.number="age"
+        >
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label>
+        <select id="city" v-model="city">
+          <option value="spb">Санкт-Петербург</option>
+          <option value="msk">Москва</option>
+          <option value="kzn">Казань</option>
+          <option value="nsk">Новосибирск</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Готов к переезду в Токио?</span>
+        <div class="checkbox">
+          <label><input type="radio" v-model="relocate" name="trip" value="yes" /> Да</label>
+        </div>
+
+        <div class="checkbox">
+          <label><input type="radio" v-model="relocate" name="trip" value="no" /> Нет</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Что знаешь во Vue?</span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" name="skills" value="vuex" /> Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" name="skills" value="cli" /> Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" name="skills" value="router" /> Vue Router</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Правила нашей компании</span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="agree" /> С правилами согласен</label>
+        </div>
+      </div>
+
+      <button type="submit" class="btn primary">Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppInput from './components/AppInput.vue'
+  export default {
+    data() {
+      return {
+        name: '',
+        age: 23,
+        city: 'spb',
+        relocate: 'yes',
+        skills: [],
+        agree: false,
+        errors: {
+          name: null
+        }
+      }
+    },
+    components: {AppInput},
+    methods: {
+      formIsValid() {
+        let isValid = true
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+        if (this.name.length === 0) {
+          this.errors.name = 'Имя не может быть пустым'
+          isValid = false
+        } else {
+          this.errors.name = null
+        }
+
+        return isValid
+      },
+      submitHandler() {
+        if (this.formIsValid()) {
+          console.group('Form Data')
+          console.log('Name:', this.name)
+          console.log('Age:', this.age)
+          console.log('City:', this.city)
+          console.log('To Tokyo:', this.relocate)
+          console.log('Skills:', this.skills)
+          console.log('Agree:', this.agree)
+          console.groupEnd()
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .form-control small {
+    color: #e53935;
+  }
+
+  .form-control.invalid input {
+    border-color: #e53935;
+  }
 </style>
